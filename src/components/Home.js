e3f32c60882c804149310c1ef4ca915839472da4
lovis-ambrose <lovisambrose212@gmail.com>
2024-09-20 18:23:00 +0300

    add, delete movie from db

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,16 @@
-import React, {useEffect, useState} from "react";
-import {account, databases} from "./Appwrite";
+import React, { useEffect, useState } from "react";
+import { account, databases } from "./Appwrite";
 import MovieList from './MovieList';
 import MovieListHeading from './MovieListHeading';
 import SearchMovie from './SearchField';
 import AddFavorite from './AddFavorite';
-import {useNavigate} from 'react-router-dom';
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import RemoveFavorites from "./RemoveFavorite";
-import {faSignOut} from "@fortawesome/free-solid-svg-icons/faSignOut";
-import {faSignIn} from "@fortawesome/free-solid-svg-icons/faSignIn";
+import { faSignOut } from "@fortawesome/free-solid-svg-icons/faSignOut";
+import { faSignIn } from "@fortawesome/free-solid-svg-icons/faSignIn";
 
 const Home = () => {
-
     const [movies, setMovies] = useState([]);
     const [favorites, setFavorites] = useState([]);
     const [searchValue, setSearchValue] = useState('');
@@ -21,16 +20,15 @@ const Home = () => {
     const databaseId = process.env.REACT_APP_MOVIES_DATABASE_ID;
     const movieCollectionId = process.env.REACT_APP_MOVIE_COLLECTION_ID;
 
-
-    const getMovieRequest = async (searchValue) => {
-        const apiUrl = `http://www.omdbapi.com/?s=${searchValue}&apikey=7b34463d`
-
-        const response = await fetch(apiUrl);
-        const responseJson = await response.json();
-        // console.log(responseJson);
-
-        if (responseJson.Search) {
-        setMovies(responseJson.Search);
+    // Fetch movies from Appwrite database
+    const fetchMoviesFromDatabase = async () => {
+        try {
+            const response = await databases.listDocuments("66eab0820029be0edb42", "66eab10c0029c603f351");
+            console.log(response.documents[0]);
+            const movieDocs = response.documents;
+            setMovies(movieDocs);  // Update state with movies from database
+        } catch (error) {
+            console.error('Error fetching movies from Appwrite', error);
         }
     };
 
@@ -40,7 +38,6 @@ const Home = () => {
             try {
                 const user = await account.get(); // Check if user session exists
                 if (user.$id) {
-                    // console.log(user);
                     setIsLoggedIn(true);
                 }
             } catch (error) {
@@ -48,66 +45,61 @@ const Home = () => {
                 setIsLoggedIn(false);
             }
         };
-
         checkUserSession();
     }, []);
 
-    // call the method only when page loads
+    // Fetch movies from OMDB API based on search value
+    const getMovieRequest = async (searchValue) => {
+        const apiUrl = `http://www.omdbapi.com/?s=${searchValue}&apikey=7b34463d`;
+        const response = await fetch(apiUrl);
+        const responseJson = await response.json();
+
+        if (responseJson.Search) {
+            setMovies(responseJson.Search);  // Update state with search results
+        }
+    };
+
+    // Fetch movies when search value changes
     useEffect(() => {
         getMovieRequest(searchValue);
     }, [searchValue]);
 
-    // retrieve movies from local storage when app loads
+    // Fetch favorite movies from the Appwrite database on load
     useEffect(() => {
-        const favoriteMovies = JSON.parse(localStorage.getItem('favorite-movie-key')) || [];
-        setFavorites(favoriteMovies);
+        fetchMoviesFromDatabase();  // Load favorite movies from Appwrite
     }, []);
 
-
-    const saveMovieToLocalStorage = (items) => {
-        localStorage.setItem('favorite-movie-key', JSON.stringify(items));
-    };
-
     const addFavoriteMovie = async (movie) => {
-        // make a copy of current list and add new movie
         const newFavoriteList = [...favorites, movie];
-        setFavorites(newFavoriteList);
+        setFavorites(newFavoriteList);  // Update favorites state
 
-        // save to local storage //
-        saveMovieToLocalStorage(newFavoriteList);
-
-        // Save the movie to AppWrite database
+        // Save movie to Appwrite database
         try {
-        await databases.createDocument(databaseId, movieCollectionId, 'unique()', {
-            title: movie.Title,
-            year: movie.Year,
-            type: movie.Type,
-            poster: movie.Poster,
-            imdbID: movie.imdbID,
-            genre: movie.Genre || "N/A",
-        });
-        console.log('Movie added to AppWrite database');
+            await databases.createDocument("66eab0820029be0edb42", "66eab10c0029c603f351", 'unique()', {
+                title: movie.Title,
+                year: movie.Year,
+                type: movie.Type,
+                poster: movie.Poster,
+                imdbID: movie.imdbID,
+                genre: movie.Genre || "N/A",
+            });
+            console.log('Movie added to Appwrite database');
         } catch (error) {
-            console.error('Error adding movie to AppWrite database', error);
+            console.error('Error adding movie to Appwrite database', error);
         }
-
     };
 
     const removeFavoriteMovie = async (movie) => {
-        // remove movie from favorites
         const newFavoriteList = favorites.filter((favoriteMovie) => favoriteMovie.imdbID !== movie.imdbID);
-        setFavorites(newFavoriteList);
+        setFavorites(newFavoriteList);  // Update favorites state
 
-        // remove from localstorage
-        saveMovieToLocalStorage(newFavoriteList);
-
-        // Remove movie from AppWrite database
+        // Remove movie from Appwrite database
         try {
-        const movieDocId = movie.imdbID; // imdbID is unique
-        await databases.deleteDocument(databaseId, movieCollectionId, movieDocId);
-        console.log('Movie removed from AppWrite database');
+            const movieDocId = movie.$id; // Use the document ID from the database
+            await databases.deleteDocument("66eab0820029be0edb42", "66eab10c0029c603f351", movieDocId);
+            console.log('Movie removed from Appwrite database');
         } catch (error) {
-            console.error('Error removing movie from AppWrite database', error);
+            console.error('Error removing movie from Appwrite database', error);
         }
     };
 
@@ -127,7 +119,7 @@ const Home = () => {
         handleNavigation('/login'); // Redirect to the login page
     };
 
-    return(
+    return (
         <div className='container-fluid'>
             <div className="d-flex justify-content-between align-items-center mt-4 mb-4 sticky-top">
                 <MovieListHeading heading="Movies"/>
@@ -144,7 +136,7 @@ const Home = () => {
                 )}
             </div>
 
-            <div className='d-flex flex-row' style={{overflowY: 'auto'}}>
+            <div className='d-flex flex-row' style={{ overflowY: 'auto' }}>
                 <MovieList movies={movies} handleFavoritesClick={addFavoriteMovie} favoriteMovie={AddFavorite}/>
             </div>
 
@@ -152,12 +144,12 @@ const Home = () => {
                 <MovieListHeading heading="Favorites"/>
             </div>
 
-            <div className='d-flex flex-row' style={{overflowY: 'auto'}}>
+            <div className='d-flex flex-row' style={{ overflowY: 'auto' }}>
                 <MovieList movies={favorites} handleFavoritesClick={removeFavoriteMovie}
                            favoriteMovie={RemoveFavorites}/>
             </div>
         </div>
     );
-}
+};
 
 export default Home;
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -5,7 +5,7 @@ const MovieList = (props) => {
 
     return (
         <>
-            {props.movies.map((movie, index) =>
+            {props.movies.map((movie) =>
                 <div className="image-container d-flex justify-content-start m-3" key={movie.imdbID}>
                     <img src={movie.Poster} alt={movie.Title}></img>
                     <div className="overlay d-flex align-items-center justify-content-center" onClick={() => props.handleFavoritesClick(movie)}>
